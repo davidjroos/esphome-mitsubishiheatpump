748d1bfc7419f1d4ee8d693927aade7aaa70d271
davidjroos <15630844+davidjroos@users.noreply.github.com>
2021-12-10 11:48:16 +1300

    Update espmhp.cpp
    two point temperature

--- components/mitsubishi_heatpump/espmhp.cpp
+++ components/mitsubishi_heatpump/espmhp.cpp
@@ -36,7 +36,7 @@ MitsubishiHeatPump::MitsubishiHeatPump(
 {
     this->traits_.set_supports_action(true);
     this->traits_.set_supports_current_temperature(true);
-    this->traits_.set_supports_two_point_target_temperature(false);
+    this->traits_.set_supports_two_point_target_temperature(true);
     this->traits_.set_visual_min_temperature(ESPMHP_MIN_TEMPERATURE);
     this->traits_.set_visual_max_temperature(ESPMHP_MAX_TEMPERATURE);
     this->traits_.set_visual_temperature_step(ESPMHP_TEMPERATURE_STEP);
